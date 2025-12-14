56bfba08d87dee0a29944949546bb39c3797e404
Sail3shp <saileshpokhrel557@gmail.com>
2025-12-14 18:52:50 +0545

    added routes

--- api/routes/blog.routes.js
+++ api/routes/blog.routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import verifyUser from '../middleware/verifyUser.js'
-import { createBlog,updateBlog,getBlogs, deleteBlog, likeBlog, unLikeBlog } from '../controller/blog.controller.js'
+import { createBlog,updateBlog,getBlogs, deleteBlog, likeBlog, unLikeBlog,getBlogsById } from '../controller/blog.controller.js'
 
 const router = express.Router()
 
@@ -9,6 +9,7 @@ router.patch('/:blogId',verifyUser,updateBlog)
 router.delete('/:blogId',verifyUser,deleteBlog)
 
 router.get('/',getBlogs)
-router.patch('/like/:blogId',verifyUser,likeBlog)
-router.patch('/unlike/:blogId',verifyUser,unLikeBlog)
+router.get('/:blogId',getBlogsById)
+router.post('/like/:blogId',verifyUser,likeBlog)
+router.delete('/unlike/:blogId',verifyUser,unLikeBlog)
 export default router
