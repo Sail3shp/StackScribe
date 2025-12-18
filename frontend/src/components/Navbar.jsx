23fd04311cca1cb130434f4592db13b62757e54a
Sail3shp <saileshpokhrel557@gmail.com>
2025-12-18 23:59:20 +0545

    mobile view

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className="w-full bg-neutral-200 ">
     <div className="flex justify-between  max-w-7xl p-6 mx-auto items-center  rounded-lg">
         <div className="font-bold text-3xl tracking-tight"><Link to={'/'}>StackScribe</Link></div>
-        <div className="md:flex hidden text-sm md:text-lg tracking-wide text-center p-1 gap-4">
+        <div className="md:flex hidden text-sm md:text-lg tracking-wide text-center  p-1 gap-4">
             <Link >Write</Link>
             <Link to={'/blogs'}>Blogs</Link>
             <Link to={'/signup'}>Sign in</Link>
@@ -16,6 +16,12 @@ const Navbar = () => {
         </div>
         <button className="md:hidden cursor-pointer" onClick={() => {setActive(!isActive)}}> <Menu />  </button>
     </div>
+    {isActive && (<div className="absolute  max-w-full  top-21   container text-sm  md:text-lg tracking-wide p-1 text-center bg-neutral-400  rounded-lg z-50 gap-4 flex flex-col ">
+            <Link >Write</Link>
+            <Link to={'/blogs'}>Blogs</Link>
+            <Link to={'/signup'}>Sign in</Link>
+            <Link to={'/login'}>Log in</Link>
+        </div>)}
     <hr></hr>
     </div>
   )
