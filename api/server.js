7ca10b24b9f7b14feb3fbad33b817556e0d4cc73
Sail3shp <saileshpokhrel557@gmail.com>
2025-12-27 08:54:08 +0545

    fixed cors

--- api/server.js
+++ api/server.js
@@ -4,10 +4,17 @@ import { connectDB } from './config/dbconfig.js'
 import userRouter from './routes/user.routes.js'
 import blogRouter from './routes/blog.routes.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
+app.use(cors(
+    {
+    origin:'http://localhost:5173',
+    credentials:true
+    }
+))
 const PORT = process.env.PORT || 9999
 app.use(express.json())
 app.use(cookieParser())
