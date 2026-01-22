4fe23da8b0265086665e4f4e0e21d974433c425c
Sail3shp <saileshpokhrel557@gmail.com>
2026-01-22 22:20:52 +0545

    added rate limiting in users

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import verifyUser from '../middleware/verifyUser.js'
 import { register,login,logout,deleteUser, updateUser } from '../controller/user.controller.js'
+import { authLimiter } from '../utils/rateLimiter.js'
 const router = express.Router()
 //auth flows
-router.post('/login',login)
-router.post('/register',register)
+router.post('/login',authLimiter,login)
+router.post('/register',authLimiter,register)
 router.post('/logout',logout)
 //user flows
 router.delete('/:userId',verifyUser,deleteUser)
