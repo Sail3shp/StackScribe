42dbaf5fcc9f8b775b7493eaf0aec43321820929
Sail3shp <saileshpokhrel557@gmail.com>
2026-02-02 12:42:46 +0545

    ui:active nav header

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
-import { Link, useNavigate } from "react-router"
+import { Link, NavLink, useNavigate } from "react-router"
 import { Menu } from 'lucide-react'
-import { useSelector,useDispatch } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { logout } from "../app/userSlice"
 const Navbar = () => {
   const [isActive, setActive] = useState(false)
@@ -9,27 +9,43 @@ const Navbar = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
-  const handleLogout = async() => {
-    await dispatch(logout())
-    navigate('/login',{replace:true})
+  const handleLogout = () => {
+    dispatch(logout())
+    navigate('/login', { replace: true })
   }
   return (
-    <div className="top-0 backdrop-blur-md w-full bg-light sticky z-10 border-b border-slate-200">
+    <div className="top-0  w-full bg-light sticky z-10 border-b border-slate-200/80">
       <div className="flex justify-between  max-w-7xl p-6 mx-auto items-center  rounded-lg">
         <div className="font-bold text-2xl tracking-tight text-slate-900"><Link to={'/'} className="text-2xl">StackScribe</Link></div>
         <div className="md:flex hidden  md:text-lg tracking-wide text-center items-center p-1 space-x-5 gap-4">
-          <Link to={'/write'} className="text-sm font-medium hover:text-primary">Write</Link>
-          <Link to={'/blogs'} className="text-sm font-medium hover:text-primary">Blogs</Link>
-          {user ? 
-          <button onClick={handleLogout} className="text-red-500 hover:underline">Log out</button>
-          :(
-            <>
-            <Link to={'/signup'} className="text-sm font-medium hover:text-primary">Sign up</Link>
-            <Link to={'/login'} className="text-sm font-medium text-white bg-slate-900 hover:opacity-90 rounded-xl px-5 py-2.5">Log in</Link>
-            </>
-          )
-        
-        }
+          <NavLink to={'/write'}
+            className={({ isActive }) =>
+              `text-sm font-medium hover:text-primary ${isActive ? 'text-sky-600' : 'text-slate-900'
+              }`
+            }>Write</NavLink>
+          <NavLink to={'/blogs'}
+            className={({ isActive }) =>
+              `text-sm font-medium hover:text-primary ${isActive ? 'text-sky-600' : 'text-slate-900'
+              }`
+            }>Blogs</NavLink>
+          {user ?
+            <button onClick={handleLogout} className="text-red-500 hover:underline">Log out</button>
+            : (
+              <>
+               <NavLink to={'/signup'}
+            className={({ isActive }) =>
+              `text-sm font-medium hover:text-primary ${isActive ? 'text-sky-600' : 'text-slate-900'
+              }`
+            }>Sign up</NavLink> 
+                <NavLink to={'/login'} 
+                className={({isActive})=>
+                `text-sm font-medium  hover:opacity-90 rounded-xl px-5 py-2.5 ${isActive?'text-slate-900 bg-sky-200':'text-white bg-slate-900'}
+                `}
+                >Log in</NavLink>
+              </>
+            )
+
+          }
 
 
         </div>
