1235aad7ca3d394be842b557e20ec108b33e706d
Sail3shp <saileshpokhrel557@gmail.com>
2026-02-07 23:00:17 +0545

    ready for deployment

--- api/server.js
+++ api/server.js
@@ -6,26 +6,39 @@ import blogRouter from './routes/blog.routes.js'
 import cookieParser from 'cookie-parser'
 import { errorHandler } from './middleware/errorHandler.js'
 import cors from 'cors'
+import path from 'path'
 
 dotenv.config()
 
 const app = express()
-app.use(cors(
-    {
-    origin:'http://localhost:5173',
-    credentials:true
-    }
-))
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors(
+        {
+            origin: 'http://localhost:5173',
+            credentials: true
+        }
+    ))
+}
+
 const PORT = process.env.PORT || 9999
+const __dirname = path.resolve()
 app.use(express.json())
 app.use(cookieParser())
 
-app.use('/api/v1/auth',userRouter)
-app.use('/api/v1/blog',blogRouter)
+app.use('/api/v1/auth', userRouter)
+app.use('/api/v1/blog', blogRouter)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get('/*splat', (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
+
 app.use(errorHandler)
 
 connectDB().then(() => {
-    app.listen(PORT,() => {
-    console.log("Server Started on port",PORT)
-})
+    app.listen(PORT, () => {
+        console.log("Server Started on port", PORT)
+    })
 })
--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 //auth,/blog
+const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:8888/api/v1' : '/api/v1'
 const api = axios.create({
-    baseURL: "http://localhost:8888/api/v1",
+    baseURL: BASE_URL,
     withCredentials:true
 })
 
