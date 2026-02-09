cb83386be9c40a6933887061251cc6ee4bd4910b
Sail3shp <saileshpokhrel557@gmail.com>
2026-02-09 18:48:24 +0545

    responsive footer&homepage

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router"
 const Footer = () => {
   return (
     <footer  className="w-full f bg-light border border-t border-slate-200">
-        <div className="flex p-6  md:text-lg text-neutral-800 max-w-7xl mx-auto justify-between">
-           <p className="text-sm">2026 Stackscribe.All rights reserved</p> 
-           <div className="flex gap-6">
+        <div className="flex flex-col-reverse md:flex-row p-6   md:text-lg text-neutral-800 max-w-7xl mx-auto justify-between">
+           <p className="text-sm text-center">© 2026 Stackscribe.All rights reserved</p> 
+           <div className="flex justify-center font-medium gap-2 md:gap-6">
             <Link>Privacy Policy</Link>
             <Link>Terms of Service</Link>
             <Link>Contact</Link>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router'
 const HomePage = () => {
     return (
         <div className='flex flex-col max-w-7xl  mx-auto bg-[radial-gradient(circle_at_center,rgba(59,130,246,0.08)_0%,transparent_70%)]'>
-            <div className='grid grid-cols-1 lg:grid-cols-2 gap-12 items-center w-full pt-10'>
+            <div className='grid grid-cols-1 px-4 gap-6 lg:grid-cols-2 md:gap-12 items-center w-full pt-10'>
                 <div className='space-y-8'>
-                    <h1 className='text-7xl lg:text-8xl tracking-tight text-slate-900 font-extrabold leading-20 font-heading '>
+                    <h1 className='text-7xl lg:text-8xl tracking-tight text-slate-900 font-extrabold leading-20 font-heading'>
                         Human
                         <br />
                         <span className='bg-linear-to-r from-primary to to-blue-600 bg-clip-text text-transparent '>stories</span> &amp;<br />
@@ -17,7 +17,7 @@ const HomePage = () => {
                     </p>
                     <div className='flex gap-8 items-center'>
                         <Link to={'/blogs'} 
-                        className='bg-[#3b82f6] font-semibold hover:bg-blue-600 py-4 px-8 text-lg text-white shadow-lg rounded-xl hover:-translate-y-1 duration-200 cursor-pointer'>
+                        className='bg-[#3b82f6] text-center font-semibold hover:bg-blue-600 py-2 md:py-4 px-8 text-lg text-white shadow-lg rounded-xl hover:-translate-y-1 duration-200 cursor-pointer'>
                         Start Reading</Link>
                         <button className='flex items-center gap-3 text-lg text-neutral-600 font-semibold'> <CirclePlay /> How it works</button>
                     </div>
@@ -34,16 +34,16 @@ const HomePage = () => {
                 </div>
                 <img src='/astronaut.png' />
             </div>
-            <div className='bg-white max-w-7xl  mx-auto mt-10 mb-10 p-12 rounded-2xl shadow-lg'>
-                <div className='grid gap-12 grid-cols-2'>
+            <div className='bg-white max-w-7xl mx-5  md:mx-auto mt-10 mb-10 p-12 rounded-2xl shadow-lg'>
+                <div className='grid gap-6 md:gap-12 grid-cols-1 md:grid-cols-2'>
                     <div className='flex flex-col'>
-                        <h1 className='font-heading font-bold tracking-wide text-slate-900 text-3xl'>Curated stories in your inbox</h1>
-                        <p>Join our newsletter to get the best of StackScribe delivered weekly to your email.</p>
+                        <h1 className='font-heading font-bold tracking-wide text-slate-900 text-xl md:text-3xl'>Curated stories in your inbox</h1>
+                        <p className=''>Join our newsletter to get the best of StackScribe delivered weekly to your email.</p>
                     </div>
-                    <div className='flex gap-4 py-3'>
-                        <input className='bg-neutral-200  rounded-2xl ps-4  w-full'
+                    <div className='flex flex-col md:flex-row gap-4 py-3'>
+                        <input className='bg-neutral-200  rounded-2xl ps-4 py-4 md:py-0 w-full'
                             placeholder='Enter your email' />
-                        <button className='bg-slate-800 text-white text-lg px-8 rounded-2xl'>Subscribe</button>
+                        <button className='bg-slate-800 py-4 md:py-0 text-white text-lg px-8 rounded-2xl'>Subscribe</button>
                     </div>
                 </div>
             </div>
